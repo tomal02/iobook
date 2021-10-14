7c9fad9b11a6d3c64585d13b00287c020b8dc217
Boris Lazarov <lazarov_borislav@yahoo.com>
2021-10-14 15:43:38 +0100

    LJB_post adding functionality update, delete, edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,18 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update]
+
 
   def new
     @post = Post.new
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to posts_url
+    @post = Post.new(post_params)
+        if @post.save
+          redirect_to posts_path
+        else
+      render :new
+        end
   end
 
   def index
@@ -14,10 +20,29 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    
+  end
+
+  def edit
+    
+  end
+
+
+  def update
+    
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
   end
 
   private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
 
   def post_params
     params.require(:post).permit(:message)
