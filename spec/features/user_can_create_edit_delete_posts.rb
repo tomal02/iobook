5607fac8b9fecb0bbbdd21b89733f354ab975b7e
Tom Lee <tomalee2002@gmail.com>
2021-10-18 16:41:10 +0100

    adds user feature tests

--- spec/features/user_can_create_edit_delete_posts.rb
+++ spec/features/user_can_create_edit_delete_posts.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'feature_helpers'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
--- spec/features/user_can_login_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-require 'feature_helpers'
-
-RSpec.feature "Logged in", type: :feature do
-  scenario "Can log in" do
-    login
-    expect(page).to have_content("Sign out")
-  end
-end
