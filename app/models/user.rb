492b20340b68e7872635f61ffe0a72f7b8f73e88
Shakira <shakira.r.salazar@gmail.com>
2021-10-21 10:51:41 +0100

    active storage install and mostly working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
+	# Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-	validates :email, email: true			 
+	validates :email, email: true	
+  has_one_attached :avatar 
 end
