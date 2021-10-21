66905aa8eae79bfa6cf331c9084348dc1d033e23
Tom Lee <tomalee2002@gmail.com>
2021-10-21 11:02:48 +0100

    edit profile - added 'About me'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :country, :languages, :ide, :github, :linkedin, :twitter, ])
   end  
 end
 
